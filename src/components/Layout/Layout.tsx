fcd5f236b50141307251f6d1c01b52f9905dae10
Mihai Herinean <mihai_herinean@yahoo.com>
2023-10-03 20:50:56 +0300

    modified layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -10,7 +10,9 @@ function Layout({children}:ILayout) {
     return (
     <div className="layout">
       <Sidebar className="layout__sidebar"/>
-      <header className="layout__header"></header>
+      <header className="layout__header">
+        <h1 className="layout__headerTitle"> {'<'}MihaiH{'/>'}</h1>
+      </header>
       <main className="layout__scroll">
         <div className="layout__content"> 
           {children}
